2b1e80b1f8fdfa95374ac16fbd55ab0d2c1410ed
Peter Niederwieser <pniederw@gmail.com>
2011-08-08 22:56:59 +0200

    undid changes to Groovy(JointCompilation)CompileOptions

--- subprojects/plugins/src/main/groovy/org/gradle/api/tasks/compile/GroovyCompileOptions.groovy
+++ subprojects/plugins/src/main/groovy/org/gradle/api/tasks/compile/GroovyCompileOptions.groovy
@@ -29,7 +29,6 @@ public class GroovyCompileOptions extends AbstractOptions {
     String encoding = null
     boolean fork = true
     GroovyForkOptions forkOptions = new GroovyForkOptions()
-    GroovyJointCompilationOptions jointCompilationOptions = new GroovyJointCompilationOptions()
     @Input
     boolean includeJavaRuntime = false
     boolean stacktrace
@@ -52,6 +51,6 @@ public class GroovyCompileOptions extends AbstractOptions {
     }
 
     Map optionMap() {
-        super.optionMap() + forkOptions.optionMap() + jointCompilationOptions.optionMap()
+        super.optionMap() + forkOptions.optionMap()
     }
 }
