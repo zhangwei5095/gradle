d4a60b6301bb2bfddb4334b5684371d6d8257676
Luke Daley <ld@ldaley.com>
2013-10-21 14:04:02 +0100

    Fix for trying to use plugins {} on a non pluggable target.

--- subprojects/core/src/integTest/groovy/org/gradle/plugin/PluginHandlerScriptIntegTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/plugin/PluginHandlerScriptIntegTest.groovy
@@ -66,7 +66,7 @@ class PluginHandlerScriptIntegTest extends AbstractIntegrationSpec {
 
         file("plugin.gradle") << """
             plugins {
-                apply plugin: "foo"
+                apply "foo"
             }
         """
 
